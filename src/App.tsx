ff7b1a6568968888887534b8aafea0a942855cee
abdulloh0512 <abdullohdev0512@gmail.com>
2024-03-09 18:16:37 +0500

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useLocalStorage } from './hook/useLocalStorage';
+import { useLocalStorage } from "./hook/useLocalStorage";
 
-import { AdvancedStatistics } from './components/AdvancedStatistics';
-import { BoostLinks } from './components/BoostLinks';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
-import { LinkShortener } from './components/LinkShortener';
-import { Presentation } from './components/Presentation';
-import { ShortenedLink } from './components/ShortenedLink';
+import { AdvancedStatistics } from "./components/AdvancedStatistics";
+import { BoostLinks } from "./components/BoostLinks";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { LinkShortener } from "./components/LinkShortener";
+import { Presentation } from "./components/Presentation";
+import { ShortenedLink } from "./components/ShortenedLink";
 
 export function App() {
-	const [links, setLinks] = useLocalStorage<string[][]>('@links', []);
+  const [links, setLinks] = useLocalStorage<string[][]>("@links", []);
 
-	return (
-		<>
-			<Header />
-			<Presentation />
-			<LinkShortener links={links} setLinks={setLinks} />
+  return (
+    <>
+      <Header />
+      <Presentation />
+      <LinkShortener links={links} setLinks={setLinks} />
 
-			{links.length > 0 && (
-				<div className="bg-neutral-light-gray">
-					<div className="pt-5 flex flex-col px-40 gap-3 mobile:px-6">
-						{links.map((link, index) => (
-							<ShortenedLink key={index} originalLink={link[0]} shortenedLink={link[1]} />
-						))}
-					</div>
-				</div>
-			)}
+      {links.length > 0 && (
+        <div className="bg-neutral-light-gray">
+          <div className="pt-5 flex flex-col px-40 gap-3 mobile:px-6">
+            {links.map((link, index) => (
+              <ShortenedLink
+                key={index}
+                originalLink={link[0]}
+                shortenedLink={link[1]}
+              />
+            ))}
+          </div>
+        </div>
+      )}
 
-			<AdvancedStatistics />
-			<BoostLinks />
-			<Footer />
-		</>
-	);
+      <AdvancedStatistics />
+      <BoostLinks />
+      <Footer />
+    </>
+  );
 }
